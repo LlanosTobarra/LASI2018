113035aae4cf80b212be01df83bd70e6bf9c7979
LlanosTobarra <llanos@scc.uned.es>
2018-06-11 13:45:57 +0200

    Versión inicial
    Inclusion DOC

---
+++ Example 1 - Twitter/twitter_scrapper_completo.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 17:21:08 2017
+
+@author: Llanos
+"""
+
+#llamamos a la librería
+import tweepy
+import json
+#creamos cuatro variables para poner nuestros códigos. 
+consumer_key=''
+consumer_secret=''
+access_token=''
+access_token_secret=''
+
+#cargamos el cliente de ElasticSearch
+from elasticsearch import Elasticsearch
+
+#creamos una clase que hereda a StreamListener
+class StreamWriter(tweepy.StreamListener):
+    def __init__(self, topic_name, *args, **kwargs):
+        tweepy.StreamListener.__init__(self,*args, **kwargs)
+        self.topic_name = topic_name
+
+        #creamos la conexión a ElasticSearch
+        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        print("Conectados a ElasticSearch")
+        print(self.es.info())
+
+        
+
+    def on_data(self, data):
+        try:
+            print(data)
+            self.es.index(index="twitter-{0}".format(self.topic_name), doc_type="tweet",body=json.loads(data))
+        except Exception as e:
+            print("Excepción durante la ejecución {0}".format(e))
+        return True
+
+
+
+#Se crea el token de autenticación
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+#obtenemos el manejador de la api
+api = tweepy.API(auth)
+
+#Abrimos un stream de Twitter usando nuestra clase
+msl= StreamWriter("uned")
+miStream = tweepy.Stream(auth = api.auth, listener=msl)
+
+#capturamos los tweets, vamos a capturar también excepciones por si acaso
+try:
+    miStream.filter(track=[u'uned',u'@uned',u'unedianos'])
+except Exception as e:
+    print("Excepción durante la ejecución {0}".format(e))
---
+++ Example 4 - syntectic data/load_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+from elasticsearch import Elasticsearch
+
+'''
+El objetivo de este script es cargar en ElasticSearch el excel ejemplo. Nos servirá como base para
+trabajar.
+El fichero que queremos cargar está en formato excel, tenemos que pasarlo a JSON
+nos apoyamos en pandas.
+'''
+#Abrimos el fichero
+data=pd.read_csv("out2.csv",sep=',')
+#cogemos la hoja del excel
+
+print("Open file")
+#exportamos el dataframe a JSON. La orientación por defecto es columnas, para que trate cada registro como un elemento
+#hay que poner el orient como records.
+tmp=df.to_json(orient='records')
+print("File to JSON")
+
+#conexión al cluster de Elasticsearh
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+print("Connected to ElasticSearch")
+print(es.info())
+#cargamos de uno en uno
+df_json= json.loads(tmp)
+print("Start loading")
+i=0
+for doc in df_json:
+    print(i)
+    es.index(index="LASI2018-lak17", doc_type="registry",body=doc)
+    i=i+1
+print("Ended")
